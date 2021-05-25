b663a702b113a687316993e453c0ad07cf9bc7a2
shogo-ppt <legonxt131317@gmail.com>
2021-05-25 17:18:10 +0900

    途中計算を表示しないバージョン01

---
+++ kiso_tyukan/matrix_renritsu_02.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  11:07:45 2021
+
+@author: legon
+"""
+
+import sympy
+
+print('--------------------------------------------------')
+print('# [[a,b],[c,d]]*[x,y]≡[e,f] mod(?)となる[x,y]を求める')
+print('--------------------------------------------------')
+
+a = int(input('a:'))
+b = int(input('b:'))
+c = int(input('c:'))
+d = int(input('d:'))
+e = int(input('e:'))
+f = int(input('f:'))
+mod = int(input('mod:'))
+print()
+
+A = sympy.Matrix([[a,b],[c,d]])
+A2 = A.det()
+
+A3 = A2 % mod
+A_1 = A.inv()
+A4 = A2 * A_1
+
+x,y,t = sympy.gcdex(A3,mod)
+A5 = x * A4
+
+a,b,c,d = A5
+a %= mod
+b %= mod
+c %= mod
+d %= mod
+
+# A6:求める逆行列
+A6 = sympy.Matrix([[a,b],[c,d]])
+a,b,c,d = A6
+
+print('求めるx,yは、')
+B1 = sympy.Matrix([e,f])
+B2 = A6 * B1
+# print(B2)
+print('[ x ]')
+print('[ y ]')
+print('≡')
+print('[',a,',',b,']')
+print('[',c,',',d,']')
+print('*')
+print('[',e,']')
+print('[',f,']')
+
+a,b = B2
+a %= mod
+b %= mod
+B3 = sympy.Matrix([a,b])
+# print(B3)
+
+print('≡')
+print('[',a,']')
+print('[',b,'] mod(',mod,')')
